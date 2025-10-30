eb236a9ceff319bbf783fb6bcc2b6d6ad8f652e7
Aidan <100976971+Borngle@users.noreply.github.com>
2025-10-30 17:59:18 +0000

    End of statement token

--- src/Language/Transform.fs
+++ src/Language/Transform.fs
@@ -118,6 +118,9 @@ module Lexer =
         | RightBracket
         | LeftBrace
         | RightBrace
+        
+        // end of statement
+        | SemiColon
 
     /// <summary>
     ///     A descriptive wrapper for a <c>Token</c> list.
@@ -250,6 +253,9 @@ module Lexer =
              | '}'        :: tail -> RightBrace         :: scan tail
              | '['        :: tail -> LeftBracket        :: scan tail
              | ']'        :: tail -> RightBracket       :: scan tail
+             
+             // end of statement
+             | ';'        :: tail -> SemiColon          :: scan tail
              
              // comment (no token just ignores)
              | '#' :: tail ->
