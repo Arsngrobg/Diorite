3fdf3e398febd997f1044087faf89c77f926886b
James Armstrong <arsngrobg@gmail.com>
2025-11-17 18:28:55 +0000

    version unversioned

---
+++ src/Diorite.CLI/Main.fs
@@ -0,0 +1,32 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+//    _____  __              __ __
+//   |     \|__|.-----.----.|__|  |_.-----.
+//   |  --  |  ||  _  |   _||  |   _|  -__|
+//   |_____/|__||_____|__|  |__|____|_____|
+//
+// ------------------------------------------------------------------------------------------------------------------
+// File:    Main.fs
+// Summary: The entry point for Diorite when ran from the user's terminal
+// Author:  Arsngrobg
+// Version: v1.1
+// ------------------------------------------------------------------------------------------------------------------
+// Developed and Created by James Armstrong (Arsngrobg) and Aidan Barden (Borngle) (2025)
+// ------------------------------------------------------------------------------------------------------------------
+
+namespace Diorite.Lang.CLI
+
+open Diorite.Lang.CLI.Configuration
+
+/// <summary>
+///     <p>The primary module of the CLI.</p>
+/// </summary>
+[<AutoOpen>]
+module Main =
+    /// <summary>
+    ///     <p>The main function.</p>
+    /// </summary>
+    /// <param name='argv'> the arguments provided to the executable </param>
+    /// <returns> an exit code that describes the state of the CLI after exiting. </returns>
+    [<EntryPoint>]
+    let main (argv: string array): int =
+        argv |> (collectArgs >> executeArgs >> int)
