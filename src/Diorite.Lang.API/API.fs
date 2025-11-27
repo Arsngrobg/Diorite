3648d5c69d0f0229b8a9aa2a9123e41901198bc9
James Armstrong <arsngrobg@gmail.com>
2025-11-27 15:55:04 +0000

    Fixing some build issues

---
+++ src/Diorite.Lang.API/API.fs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+//    _____  __              __ __
+//   |     \|__|.-----.----.|__|  |_.-----.
+//   |  --  |  ||  _  |   _||  |   _|  -__|
+//   |_____/|__||_____|__|  |__|____|_____|
+//
+// ------------------------------------------------------------------------------------------------------------------
+// File:    API.fs
+// Summary: The public-facing API for the Diorite language - the internal function bindings
+// Author:  Arsngrobg
+// Version: v1.0
+// ------------------------------------------------------------------------------------------------------------------
+// Developed and Created by James Armstrong (Arsngrobg) and Aidan Barden (Borngle) (2025)
+// ------------------------------------------------------------------------------------------------------------------
+
+module Diorite.Lang.API
+
+open Diorite.Lang.Core
+
+[<AutoOpen>]
+module API =
+    let getVersion (): Properties.Version =
+        Properties.languageVersion
