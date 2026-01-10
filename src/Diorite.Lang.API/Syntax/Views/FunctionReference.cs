070570d2941d9e69c649daab56f08dece3604afd
James Armstrong <arsngrobg@gmail.com>
2026-01-10 00:41:48 +0000

    Move sealed subclasses to bottom of file for easier reading

--- src/Diorite.Lang.API/Syntax/Views/FunctionReference.cs
+++ src/Diorite.Lang.API/Syntax/Views/FunctionReference.cs
@@ -19,7 +19,7 @@ using Diorite.Lang.API.Traits;
 namespace Diorite.Lang.API.Syntax.Views;
 
 public abstract class FunctionReference : ICoreView<Core.Syntax.FunctionReferenceType>,
-                                              ICoreConverter<Core.Syntax.FunctionReferenceType, FunctionReference>
+                                          ICoreConverter<Core.Syntax.FunctionReferenceType, FunctionReference>
 {
     public static FunctionReference OfCoreType(Core.Syntax.FunctionReferenceType coreRef) => coreRef switch
     {
@@ -27,6 +27,27 @@ public abstract class FunctionReference : ICoreView<Core.Syntax.FunctionReferenc
         Core.Syntax.FunctionReferenceType.OfSymbol   symRef => new OfSymbol(symRef.Item),
         _ => throw new InvalidOperationException("All match cases were not covered")
     };
+
+    /// <summary>
+    ///     <p>Performs pattern matching on this <c>FunctionReference</c>.</p>
+    /// </summary>
+    /// <param name="onVariable"> the callback to execute if this <c>FunctionReference</c> is of a variable </param>
+    /// <param name="onSymbol"> the callback to execute if this <c>FunctionReference</c> is of  symbol </param>
+    /// <typeparam name="T"> the result of this pattern matching operation </typeparam>
+    /// <returns> the result of the pattern match, bound by the type <c>T</c> </returns>
+    public T Match<T>(Func<Variable, T> onVariable, Func<string, T> onSymbol) => this switch
+    {
+        OfVariable varRef => onVariable(varRef.Variable),
+        OfSymbol symRef   => onSymbol  (symRef.Symbol),
+        _                 => throw new InvalidOperationException($"Invalid case {GetType()}")
+    };
+    
+    private FunctionReference() {}
+
+    public abstract Core.Syntax.FunctionReferenceType AsCoreType();
+    public abstract override int    GetHashCode();
+    public abstract override bool   Equals(object? obj);
+    public abstract override string ToString();
 
     /// <summary>
     ///     <p>The case of <c>FunctionReference</c> where a <i>variable reference</i> is used when invoking a
@@ -85,25 +106,4 @@ public abstract class FunctionReference : ICoreView<Core.Syntax.FunctionReferenc
         public override string ToString() =>
             $"SymbolicFunctionReference[{Symbol}]";
     }
-
-    /// <summary>
-    ///     <p>Performs pattern matching on this <c>FunctionReference</c>.</p>
-    /// </summary>
-    /// <param name="onVariable"> the callback to execute if this <c>FunctionReference</c> is of a variable </param>
-    /// <param name="onSymbol"> the callback to execute if this <c>FunctionReference</c> is of  symbol </param>
-    /// <typeparam name="T"> the result of this pattern matching operation </typeparam>
-    /// <returns> the result of the pattern match, bound by the type <c>T</c> </returns>
-    public T Match<T>(Func<Variable, T> onVariable, Func<string, T> onSymbol) => this switch
-    {
-        OfVariable varRef => onVariable(varRef.Variable),
-        OfSymbol symRef   => onSymbol  (symRef.Symbol),
-        _                 => throw new InvalidOperationException($"Invalid case {GetType()}")
-    };
-    
-    private FunctionReference() {}
-
-    public abstract Core.Syntax.FunctionReferenceType AsCoreType();
-    public abstract override int    GetHashCode();
-    public abstract override bool   Equals(object? obj);
-    public abstract override string ToString();
 }
