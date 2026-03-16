ba578b9bce85013b46ced74b5f9d18a74a4e70f3
James Armstrong <arsngrobg@gmail.com>
2026-03-16 06:36:17 +0000

    scratch(refactor): `CAL` opcode + renaming of opcodes

--- src/scratch.c
+++ src/scratch.c
@@ -6,81 +6,77 @@
 // the 'reg' & 'tmp' cannot be access through the high level programming interface
 // INSPIRED A LOT BY: https://imomath.com/index.cgi?page=asmNotesFunctions
 
-#include <ctype.h>
+// this is an incredibly verbose implementation of a virtual machine for a language
+// aggressive optimizations will come later
+// implementation over strategy
+
 #include <stdint.h>
 
-#define DVM_INSTARGS      (3)
-#define DVM_STACKSIZE     (4 << 20)
-#define DVM_SPREGCOUNT    (2)                   /* return (ret) & temporary (tmp) register        */
-#define DVM_GPREGCOUNT    ((26 * 2) * (1 + 10)) /* (upper + lower) * (no subscript + subscripted) */
-#define DVM_INT(x)        (DVM_Literal) { .type = DVM_LITERAL_INTEGER,  .as.integer = (x)          }
-#define DVM_DECIMAL(x)    (DVM_Literal) { .type = DVM_LITERAL_DECIMAL,  .as.decimal = (x)          }
-#define DVM_COMPLEX(a, b) (DVM_Literal) { .type = DVM_LITERAL_COMPLEX,  .as.complex = { (a), (b) } }
-#define DVM_UNDEFINED     (DVM_Literal) { .type = DVM_LITERAL_UNDEFINED                            }
-#define DVM_RETREG        (0)
-#define DVM_TMPREG        (1)
+#define DVM_INSTARGC (3)                   /* the max number of args a dyorite vm has */
+#define DVM_USRREGC  ((26 * 2) * (1 + 10)) /* the number of user registers available  */
+#define DVM_STCKSIZE (4 << 20)             /* the max stack size of the dyorite vm    */
 
-/* an atomic value in the dyorite language */
+/* a dyorite literal value */
 typedef struct {
     enum {
         DVM_LITERAL_INTEGER,
         DVM_LITERAL_DECIMAL,
         DVM_LITERAL_COMPLEX,
         DVM_LITERAL_UNDEFINED
-    } type;
+    } tag;
     union {
-        int64_t                 integer; /* DVM_LITERAL_INTEGER */
-        double                  decimal; /* DVM_LITERAL_DECIMAL */
-        struct { double a, b; } complex; /* DVM_LITERAL_COMPLEX */
+        int64_t                 integer;
+        double                  decimal;
+        struct { double a, b; } complex;
     } as;
 } DVM_Literal;
 
-/* key:
-   r  = register
-   l  = literal
-   rl = register | literal
-   i  = instruction
+/* dyorite vm opcodes
+   key:
+    r  = register
+    l  = literal
+    rl = register | literal
+    i  = instruction pointer
 */
 typedef enum {
-    DVM_SET, /* SET r , rl     */
-    DVM_GET, /* GET r          */
+    DVM_GET, /* GET r ,        */
+    DVM_SET, /* SET rl, rl,    */
     DVM_ADD, /* ADD r , rl, rl */
     DVM_SUB, /* SUB r , rl, rl */
     DVM_MUL, /* MUL r , rl, rl */
     DVM_DIV, /* DIV r , rl, rl */
     DVM_FDV, /* FDV r , rl, rl */
     DVM_MOD, /* MOD r , rl, rl */
-    DVM_EXP, /* EXP r , rl, rl */
+    DVM_POW, /* EXP r , rl, rl */
     DVM_FCT, /* FCT r , rl, rl */
-    DVM_CMP, /* CMP rl         */
-    DVM_EQL, /* EQL rl, rl, i  */
-    DVM_NEQ, /* NEQ rl, rl, i  */
-    DVM_LTN, /* LTN rl, rl, i  */
-    DVM_LTE, /* LTE rl, rl, i  */
-    DVM_GTN, /* GTN rl, rl, i  */
-    DVM_GTE, /* GTE rl, rl, i  */
+    DVM_BEQ, /* BEQ i , rl, rl */
+    DVM_BNE, /* BNE i , rl, rl */
+    DVM_BLT, /* BLT i , rl, rl */
+    DVM_BLE, /* BLE i , rl, rl */
+    DVM_BGT, /* BGT i , rl, rl */
+    DVM_BGE, /* BGE i , rl, rl */
     DVM_JMP, /* JMP i          */
+    DVM_CAL, /* CAL i          */
     DVM_RET, /* RET            */
     DVM_PSH, /* PSH rl         */
     DVM_POP  /* POP r          */
 } DVM_OpCode;
 
-/* a dyorite vm instruction:
-   Op<ID> [r|l|rl|ip]*
-*/
+/* a dyorite vm instruction */
 typedef struct {
-    DVM_OpCode op;
+    DVM_OpCode code;
     struct {
         enum {
-            DVM_REG,
-            DVM_LIT,
-            DVM_INS
-        } type;
+            DVM_ARG_LITERAL,
+            DVM_ARG_REGISTER,
+            DVM_ARG_INSTRUCTION
+        } tag;
         union {
-            uint64_t    r;
-            DVM_Literal l;
-        } payload;
-    } args[DVM_INSTARGS];
+            DVM_Literal lit; /* DVM_ARG_LITERAL     */
+            uint16_t    reg; /* DVM_ARG_REGISTER    */
+            uint64_t    ins; /* DVM_ARG_INSTRUCTION */
+        } as;
+    } args[DVM_INSTARGC];
 } DVM_Instruction;
 
 /* a dyorite vm program */
@@ -89,25 +85,20 @@ typedef struct {
     DVM_Instruction instructions[];
 } DVM_Program;
 
-/* dyorite vm state */
+/* a dyorite vm state */
 typedef struct {
-    DVM_Literal gpreg[DVM_GPREGCOUNT]; /* user registers (x, A0, p9, etc...) */
-    DVM_Literal spreg[DVM_SPREGCOUNT]; /* internal registers (ret, tmp)      */
     struct {
-        uint64_t offset;
-        uint8_t  bytes[DVM_STACKSIZE];
+        uint64_t    ip;               /* instruction pointer */
+        DVM_Literal ret;              /* return    register  */
+        DVM_Literal tmp;              /* temporary register  */
+        DVM_Literal usr[DVM_USRREGC]; /* user      registers */
+    } registers;
+    struct {
+        uint64_t top;
+        uint8_t  bytes[DVM_STCKSIZE];
     } stack;
     DVM_Program *prog;
 } DVM;
-
-// TODO: don't actually know if this works, do test this
-/* gets the equivalent register for the literal representation of a dyorite variable */
-uint64_t DVM_Reg(char c, uint8_t subscript) {
-    assert(isalpha(c));
-    uint64_t reg = isupper(c) ? (('Z' - c) + 26) : ('z' - c);
-    reg *= 11;
-    return reg + subscript;
-}
 
 // Code Snippet Example:
 // -----------------------------------------------------------------------------
@@ -135,27 +126,23 @@ uint64_t DVM_Reg(char c, uint8_t subscript) {
 //
 // fn_quad: ; quad(x) = x^2 + 2*x + 1
 //     EXP ret x_     INT(2)
-//     MUL tmp INT(2)
+//     MUL tmp INT(2) x_
 //     ADD ret ret    tmp
 //     ADD ret ret    INT(1)
 //     RET
 //
 // fn_factorial: ; factorial(n) = { undefined if n < 0 ... }
-//     CMP n_
-//     GTE INT(0) [fn_factorial_if1]
+//     BGT n_ INT(0) [fn_factorial_if1]
 //     SET ret UNDEFINED
 //     JMP [fn_factorial_ret]
 // fn_factorial_if1: ; factorial(n) = { ... 1 if n < 2 ... }
-//     CMP n_
-//     GTE INT(2) [fn_factorial_if2]
+//     BGT n_ INT(2) [fn_factorial_if2]
 //     SET ret INT(1)
 //     JMP [fn_factorial_ret]
 // fn_factorial_if2: ; factorial(n) = { ... n * f(n - 1) otherwise }
 //     PSH n_
-//     PSH [fn_factorial_call1]
 //     SUB n_  n_  1
-//     JMP [fn_factorial]
-// fn_factorial_call1:
+//     CAL [fn_factorial]
 //     POP n_
 //     MUL ret n_  ret
 // fn_factorial_ret:
@@ -163,9 +150,7 @@ uint64_t DVM_Reg(char c, uint8_t subscript) {
 //
 // outer1:
 //     PSH n_
-//     PSH [fn_factorial_call2]
 //     SET n_  INT(100)
-//     JMP [fn_factorial]
-// fn_factorial_call2:
+//     CAL [fn_factorial]
 //     POP n_
 //     SET x_  ret
